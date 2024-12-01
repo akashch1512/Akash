839475a82f7b7faa6b03cac6ed0b3fb13b9ac7ac
akash <akashchaudhari1012@gmail.com>
2024-12-01 10:08:59 +0530

    filesupadted

--- CrystalSource/CrystalSource/settings.py
+++ CrystalSource/CrystalSource/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ae68vnuo1dnjmy=)x5+&6hg0n#847kb-b@-p2!%_js$tm7$m6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
